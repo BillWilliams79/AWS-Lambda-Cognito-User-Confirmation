3e99e405f674105ec7436d9ab442a4e3791edd82
Bill Williams <divebilldive@gmail.com>
2022-07-10 16:07:34 -0700

    MySQL session configuration is not required for this simple use case, removed

--- lambda_function.py
+++ lambda_function.py
@@ -11,26 +11,6 @@ connection = pymysql.connect(host = endpoint,
                              password = password,
                              database = db,)
 
-cursor = connection.cursor()
-
-try:
-    # default session value "read repeatable" breaks ability to see
-    # updates from back end...READ COMITTED enable select to return all 
-    # committed data from all sources
-    sql_statement = "SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED"
-    no_data = cursor.execute(sql_statement)
-
-except pymysql.Error as e:
-    errorMsg = f"Set session isolation read committed failed: {e.args[0]} {e.args[1]}"
-    print(errorMsg)
-
-try:
-    # Required to allow large data returns for reads using group_concat_max_len
-    cursor.execute("SET SESSION group_concat_max_len = 65536")
-except pymysql.Error as e:
-    errorMsg = f"Set session group_concat_max_len 64k failed: {e.args[0]} {e.args[1]}"
-    print(errorMsg)
-
 varDump(connection, 'Lambda Init: connection details')
 
 
